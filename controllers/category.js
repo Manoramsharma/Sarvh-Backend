3d57a4feb219fb21e44974da38572e717fc3278f
ayshubham <71246748+ayshubham@users.noreply.github.com>
2021-08-09 11:32:28 +0530

    category

--- controllers/category.js
+++ controllers/category.js
@@ -1,34 +1,54 @@
-const slugify = require('slugify')
-const Category = require('../models/category')
+const slugify = require("slugify");
+const Category = require("../models/category");
 
-exports.addCategory =  (req, res) => {
 
-    const categoryObj = {
-        name: req.body.name,
-        slug: slugify(req.body.name)
-    }
-    if (req.body.parentId){
-        categoryObj.parentId = req.body.parentId
-    }
 
-    const cat = new Category(categoryObj);
+function createCategories(categories, parentId = null) {
+  const categoryList = [];
+  let category;
+  if (parentId == null) {
+    category = categories.filter((cat) => cat.parentId == undefined);
+  } else {
+    category = categories.filter((cat) => cat.parentId == parentId);
+  }
+  for(let cate of category){
+      categoryList.push({
+          _id: cate._id,
+          name: cate.name,
+          slug: cate.slug,
+          childern: createCategories(categories, cate._id)
+      })
+  }
 
-    cat.save((error, category) => {
-        if(error) return res.status(400).json({ error })
-        if (category){
-            return res.status(201).json({category})
-        }
-    });
-
+  return categoryList
 }
 
-exports.getCategories = (req,res) => {
-    Category.find({})
-    .exec((error, categories) => {
-        if(error) return res.status(400).json({ error })
+exports.addCategory = (req, res) => {
+  const categoryObj = {
+    name: req.body.name,
+    slug: slugify(req.body.name),
+  };
+  if (req.body.parentId) {
+    categoryObj.parentId = req.body.parentId;
+  }
 
-        if(categories){
-            res.status(200).json({categories})
-        }
-    })
-}
+  const cat = new Category(categoryObj);
+
+  cat.save((error, category) => {
+    if (error) return res.status(400).json({ error });
+    if (category) {
+      return res.status(201).json({ category });
+    }
+  });
+};
+
+exports.getCategories = (req, res) => {
+  Category.find({}).exec((error, categories) => {
+    if (error) return res.status(400).json({ error });
+
+    if (categories) {
+      const categoryList = createCategories(categories);
+      res.status(200).json({ categoryList });
+    }
+  });
+};
